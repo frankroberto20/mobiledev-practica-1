aa984c1543922aca962057e80ed417f7fec9fdba
FRANK ROBERTO CHAVEZ SOSA <1085268@est.intec.edu.do>
2021-08-14 15:10:28 -0400

    First commit

---
+++ Login/Login/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Login
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            if (username.Text == null || username.Text == "" || password.Text == null || password.Text == "")
+            {
+                await DisplayAlert ("Error", "User and Password fields can't be empty", "OK");
+            }
+            else
+            {
+                await DisplayAlert ("Welcome!", $"Hello {username.Text}", "OK");
+            }
+        }
+    }
+}
